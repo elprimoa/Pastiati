6902a6f3f722cd92788a14fa1fa1b3d5cdc13d5e
Luis Arguello <el.primo.arguello@gmail.com>
2015-02-20 10:13:25 -0430

    candadito

--- js/load.js
+++ js/load.js
@@ -5,12 +5,17 @@ function load(){
 	    for(i=idx;i<idx+5;i++){
 	    	id = data[i].id;
 	    	$(".panel-group").append('<div class="panel panel-default"><div id="p'+id+'" class="panel-heading"><h4 id="pp' + id + '" class="panel-title">');
-	    	$("#pp"+id).append('<a data-toggle="collapse" data-parent="#accordion" href="#pastie' + data[i].id + '"> <b>'+ data[i].title +'</b>, ' + data[i].owner + ' <br><br><i class="resumen">' + data[i].content.substring(0, 100) + '...</i></a>');
+	    	if(data[i].private) {
+	    		$("#pp"+id).append('<a data-toggle="collapse" data-parent="#accordion" href="#pastie' + data[i].id + '"> <b>'+ data[i].title +'</b>, ' + data[i].owner + '</a> <img src="media/lock.png"/><br><br><i class="resumen" id="preview' + data[i].id + '">' + data[i].content.substring(0, 100) + '...</i>');
+	    	}
+	    	else {
+	    		$("#pp"+id).append('<a data-toggle="collapse" data-parent="#accordion" href="#pastie' + data[i].id + '"> <b>'+ data[i].title +'</b>, ' + data[i].owner + '</a><br><br><i class="resumen" id="preview' + data[i].id + '">' + data[i].content.substring(0, 100) + '...</i>');
+	    	}
 	    	console.log($("#pp"+id));
 			$('#p'+id).append('<div id="pastie'+data[i].id+'" class="panel-collapse collapse"><div id="dv' + id + '" class="panel-body">');
 	        $('#dv' + id).append('<p>' + data[i].content + '</p><br>');
-	        $('#dv' + id).append('<p>Owner: ' + data[i].owner + '</p>');
-	        $('#dv' + id).append('<p>Private: ' + data[i].private + '</p>');
+	        $('#dv' + id).append('<p><b>Owner:</b> ' + data[i].owner + '</p>');
+	        
 	    }
 	    idx=idx+5;
 	    $("#morePasties").blur();
