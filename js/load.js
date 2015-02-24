2bae2f63898dbfdc2e975ee086e56d1487800537
Luis Arguello <el.primo.arguello@gmail.com>
2015-02-24 15:58:56 -0430

    Cambios

--- js/load.js
+++ js/load.js
@@ -1,5 +1,19 @@
 idx=0;
 
+$("#email-field").keypress(function(event) {
+    if (event.which == 13) {
+        event.preventDefault();
+        login();
+    }
+});
+
+$("#pass-field").keypress(function(event) {
+    if (event.which == 13) {
+        event.preventDefault();
+        login();
+    }
+});
+
 function load(){
 	$.getJSON("pasties.json", function(data) {
 	    for(i=idx;i<idx+5;i++){
@@ -29,10 +43,23 @@ function login() {
 		return;
 	}
 	$("#login-form").remove();
-	$("#navbar").append('<ul class="nav navbar-nav navbar-right" id="username"><li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><b>' + email + '</b> <span class="caret"></span></a><ul class="dropdown-menu" role="menu"><li><a href="#">Crear Pastie</a></li><li><a href="#">Perfil</a></li><li><a href="#" onclick="logout()">Log out</a></li></ul></li></ul>');
+	$("#navbar").append('<ul class="nav navbar-nav navbar-right" id="username"><li><a href="#create">Crear Pastie</a></li><li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false"><b>' + email + '</b> <span class="caret"></span></a><ul class="dropdown-menu" role="menu"><li><a href="#">Perfil</a></li><li><a href="#" onclick="logout()">Log out</a></li></ul></li></ul>');
 }
 
 function logout() {
 	$("#username").remove();
-	$("#navbar").append('<form class="navbar-form navbar-right" id="login-form"><div class="form-group"><input type="text" placeholder="Email" class="form-control" id="email-field"> </div> <div class="form-group"> <input type="password" placeholder="Password" class="form-control" id="pass-field"> </div> <button type="button" class="btn btn-success" onclick="login()">Log in</button></form>');
+	$("#navbar").append('<form class="navbar-form navbar-right" id="login-form"><div class="form-group has-feedback" id="email-div"><input type="text" placeholder="Email" class="form-control" id="email-field"> </div> <div class="form-group has-feedback"> <input type="password" placeholder="Password" class="form-control" id="pass-field"> </div> <button type="button" class="btn btn-success" onclick="login()">Log in</button></form>');
+	$("#email-field").keypress(function(event) {
+  	if (event.which == 13) {
+        event.preventDefault();
+        login();
+    }
+	});
+
+	$("#pass-field").keypress(function(event) {
+    if (event.which == 13) {
+        event.preventDefault();
+        login();
+    }
+	});
 }
