f1644c6598bdf8e50b81333bd2ec8aee8a4dd0ac
root <el.primo.arguello@gmail.com>
2015-04-02 12:47:38 -0430

    Forgot complete

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 from flask import *
-from flask_mail import Mail
+from flask_mail import *
 from models.pasties import *
+from random import *
+
+MAIL_SERVER = 'smtp.googlemail.com'
+MAIL_PORT = 465
+MAIL_USE_SSL = True
+MAIL_USERNAME = 'ppastiati@gmail.com'
+MAIL_PASSWORD = 'ati*pastiati'
 
 app = Flask (__name__, template_folder = 'views', static_folder = 'statics')
+app.config.from_object(__name__)
 mail = Mail(app)
 
 # Routes goes here
@@ -34,6 +42,23 @@ def pedro():
 @app.route('/forgot')
 def forgot():
   return render_template('forgot.html')
+
+@app.route('/forgotmail', methods = ['POST'])
+def forgotmail():
+  email = request.form['email']
+  msg = Message('Pastiati mail system', sender=("Pastiati", "ppastiati@gmail.com"), recipients = [email])
+  genkey = ""
+  for i in range(5):
+    genkey = genkey + chr(randint(97, 122))
+  for i in range(3):
+    genkey = genkey + chr(randint(48, 57))
+  for i in range(2):
+    genkey = genkey + chr(randint(35, 47))
+  genkey = ''.join(sample(genkey, len(genkey)))
+  msg.body = "Your new password is: %s" % genkey 
+  with app.app_context():
+    mail.send(msg)
+  return redirect(url_for('home'))
 
 if __name__ == '__main__':
   app.debug = True
